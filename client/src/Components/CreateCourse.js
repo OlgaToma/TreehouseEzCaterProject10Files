bd4bda493c30f542220b7ed74be963ac190b1493
OlgaToma <56312261+OlgaToma@users.noreply.github.com>
2021-02-16 19:00:35 -0500

    use "value" instead of "defaultValue"
    Update course create and update views to use value instead of defaultValue

--- client/src/Components/CreateCourse.js
+++ client/src/Components/CreateCourse.js
@@ -80,10 +80,10 @@ export default class CourseCreate extends Component {
                 <div className="grid-66">
                     <div className="course--header">
                         <h4 className="course--label">Course</h4>
-                        <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." onChange={this.change} defaultValue={course.title}/></div>
+                        <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." onChange={this.change} value={course.title}/></div>
                     </div>
                     <div className="course--description">
-                        <div><textarea id="description" name="description" className="" placeholder="Course description..." onChange={this.change} defaultValue={course.description}/></div>
+                        <div><textarea id="description" name="description" className="" placeholder="Course description..." onChange={this.change} value={course.description}/></div>
                     </div>
                 </div>
                 <div className="grid-25 grid-right">
@@ -91,11 +91,11 @@ export default class CourseCreate extends Component {
                         <ul className="course--stats--list">
                             <li className="course--stats--list--item">
                                 <h4>Estimated Time</h4>
-                                <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" onChange={this.change} defaultValue={course.estimatedTime}/></div>
+                                <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" onChange={this.change} value={course.estimatedTime}/></div>
                             </li>
                             <li className="course--stats--list--item">
                                 <h4>Materials Needed</h4>
-                                <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." onChange={this.change} defaultValue={course.materialsNeeded} /></div>
+                                <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." onChange={this.change} value={course.materialsNeeded} /></div>
                             </li>
                         </ul>
                     </div>
